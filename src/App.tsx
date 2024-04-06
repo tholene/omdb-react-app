6e0d6e54664659decab8aa9d56e8884c9adbfa21
Erik Tholén <tholenerik@gmail.com>
2024-04-06 09:36:34 +0200

    Add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <h1>Hello World!</h1>
+    </AppContainer>
   );
 }
 
